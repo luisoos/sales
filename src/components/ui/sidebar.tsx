15039f07c12281c378cc63556ce638b4c77adaa1
Luis <luisoos@users.noreply.github.com>
2025-08-21 16:10:15 +0200

    🐛 fix: nextjs/radix ui sidebar error

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -10,7 +10,7 @@ import { cn } from '~/lib/utils';
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import { Separator } from '~/components/ui/separator';
-import { Sheet, SheetContent } from '~/components/ui/sheet';
+import { Sheet, SheetContent, SheetTitle } from '~/components/ui/sheet';
 import { Skeleton } from '~/components/ui/skeleton';
 import {
     Tooltip,
@@ -215,6 +215,7 @@ const Sidebar = React.forwardRef<
                             } as React.CSSProperties
                         }
                         side={side}>
+                        <SheetTitle className="sr-only">Sidebar</SheetTitle>
                         <div className='flex h-full w-full flex-col'>
                             {children}
                         </div>
